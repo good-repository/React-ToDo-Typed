3b1f3621a55fdea3b2c16ff431c6fb75c875818c
Guilheme Fünkler Borelli <borelli.guilherme@gmail.com>
2021-01-04 15:06:47 -0300

    table style

---
+++ frontend/src/components/Table/styles.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+export const Styles = styled.div`
+  padding: 2rem;
+
+  table {
+    border-radius: 1rem;
+    background-color: #fff;
+    border-spacing: 0;
+    width: 100%;
+    tr {
+      height: 3rem;
+
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    tbody {
+      tr {
+        :hover {
+          background-color: #eee;
+
+          &:last-child {
+            border-radius: 1rem;
+            overflow: hidden;
+            td {
+              &:first-of-type {
+                border-bottom-left-radius: 1rem;
+              }
+              &:last-of-type {
+                border-bottom-right-radius: 1rem;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid #ccc;
+      text-align: center;
+
+      :first-child {
+        width: 60%;
+      }
+    }
+
+    td {
+      :first-child {
+        text-align: start;
+      }
+    }
+  }
+`;
