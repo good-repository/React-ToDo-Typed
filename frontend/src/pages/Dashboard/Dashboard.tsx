8ed64adc67ff95cac6682c7fc57ffb6510a36b70
Guilheme Fünkler Borelli <borelli.guilherme@gmail.com>
2021-01-05 20:22:30 -0300

    form styling

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,48 @@
 import { useMemo, useEffect, useState } from "react";
-import { Button } from "./styles";
+import { Button, Container, Form, Input, Label, Title } from "./styles";
 import { Table } from "../../components/Table";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import Select from "react-select";
 
-import { MdCheck, MdDelete, MdEdit, MdUndo } from "react-icons/md";
+import { MdCheck, MdDelete, MdEdit, MdSearch, MdUndo } from "react-icons/md";
+
+const selectStyles = {
+  option: (provided: any, state: any) => ({
+    ...provided,
+    backgroundColor: state.isSelected
+      ? "rgba(0, 0, 0, 0.4)"
+      : "rgba(0, 0, 0, 0.1)",
+    borderBottom: "1px solid white",
+  }),
+  control: (provided: any) => ({
+    ...provided,
+    backgroundColor: "rgba(0, 0, 0, 0.1)",
+    fontSize: "1.2rem",
+    border: "none",
+    borderRadius: "10px",
+  }),
+};
 
 export default function Dashboard() {
   const [list, setList] = useState([]);
+  const [options, setOptions] = useState([]);
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
 
   useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) =>
+        setOptions(
+          json.map((item: any) => {
+            return {
+              value: item.id,
+              label: item.name,
+            };
+          })
+        )
+      );
+
     fetch("https://jsonplaceholder.typicode.com/todos")
       .then((response) => response.json())
       .then((json) =>
@@ -83,5 +118,68 @@ export default function Dashboard() {
     []
   );
 
-  return <Table columns={columns} data={list} />;
+  return (
+    <Container>
+      <Title>Task Form</Title>
+      <Form>
+        <Label>Description</Label>
+        <div style={{ display: "flex", flexGrow: 1, marginBottom: "1rem" }}>
+          <Input />
+          <Button style={{ backgroundColor: "#17a2b8" }}>
+            <MdSearch size={20} color={"#fff"} />
+          </Button>
+        </div>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div style={{ display: "flex" }}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                marginRight: "1rem",
+              }}
+            >
+              <Label>Date</Label>
+              <DatePicker
+                selected={startDate}
+                onChange={(date: Date | null) => setStartDate(date)}
+                dateFormat="dd/MM/yyyy"
+                customInput={<Input />}
+              />
+            </div>
+            <div style={{ width: "260px" }}>
+              <Label>Responsible</Label>
+              <Select options={options} styles={selectStyles} />
+            </div>
+          </div>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Button
+              style={{
+                backgroundColor: "#28a745",
+                width: "100px",
+                color: "white",
+                fontWeight: "bold",
+                letterSpacing: "1px",
+              }}
+            >
+              ADD
+            </Button>
+            <Button
+              style={{
+                backgroundColor: "#dc3545",
+                width: "100px",
+                color: "white",
+                fontWeight: "bold",
+                letterSpacing: "1px",
+              }}
+            >
+              CLEAR
+            </Button>
+          </div>
+        </div>
+      </Form>
+
+      <Title>To Do List</Title>
+      <Table columns={columns} data={list} />
+    </Container>
+  );
 }
--- frontend/src/pages/Dashboard/styles.js
+++ frontend/src/pages/Dashboard/styles.js
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+  padding: 2rem;
+`;
+
 export const Button = styled.button`
   margin-left: 10px;
   border-radius: 10px;
@@ -15,4 +19,36 @@ export const Button = styled.button`
   :first-child {
     margin-left: 0;
   }
+`;
+
+export const Form = styled.form`
+  background-color: white;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+  margin-bottom: 2rem;
+`;
+
+export const Input = styled.input`
+  flex-grow: 1;
+  height: 2.5rem;
+  border-radius: 10px;
+  border: none;
+  font-size: 1.2rem;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding-left: 10px;
+`;
+
+export const Label = styled.label`
+  font-size: 1.2rem;
+  font-weight: 700;
+`;
+
+export const Title = styled.p`
+  font-size: 2.5rem;
+  border-bottom: 1px solid white;
+  color: white;
+  padding: 1rem 0;
+  margin-bottom: 2rem;
 `;
